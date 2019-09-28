0432ea040a5b7c09d3265ebfd7d745a12a00e111
Matthew Barber <quitesimplymatt@gmail.com>
2019-09-28 16:41:26 +0100

    unit 1 of softtware proj management

---
+++ CS3360 Software Project Management/mindmap.mm
@@ -0,0 +1,295 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Software Project Management" FOLDED="false" ID="ID_1865783124" CREATED="1569680420659" MODIFIED="1569680740432" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.712">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Why?" POSITION="right" ID="ID_241605968" CREATED="1569680836843" MODIFIED="1569681229435">
+<edge COLOR="#ff0000"/>
+<node TEXT="Projects go haywire" ID="ID_427124072" CREATED="1569681186254" MODIFIED="1569681745321">
+<hook URI="img/how-customer-explained.jpeg" SIZE="0.048213862" NAME="ExternalObject"/>
+<node ID="ID_1132772853" CREATED="1569680848420" MODIFIED="1569681173459"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Help customer understand what their <i>real</i>&#160;needs are
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Understand the different points of views from different stakeholders" ID="ID_326338746" CREATED="1569681236470" MODIFIED="1569681319772"/>
+</node>
+<node TEXT="Why software-specific project management?" ID="ID_1563904501" CREATED="1569681885941" MODIFIED="1569681897226">
+<node TEXT="High number of unsuccessful, late or/and over-budget projects" ID="ID_17851668" CREATED="1569682198970" MODIFIED="1569682230624">
+<node TEXT="No physical artefacts" ID="ID_749370391" CREATED="1569682055788" MODIFIED="1569682160125"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ?
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Past projects carry over limitied learnt knowledge" ID="ID_841860520" CREATED="1569681956629" MODIFIED="1569681979462">
+<node TEXT="No need to create games twice" ID="ID_704896343" CREATED="1569681988173" MODIFIED="1569682021922">
+<hook NAME="AlwaysUnfoldedNode"/>
+</node>
+</node>
+<node TEXT="Rapid change" ID="ID_1702720810" CREATED="1569681944774" MODIFIED="1569681950507"/>
+<node TEXT="New field" ID="ID_154051762" CREATED="1569681902743" MODIFIED="1569681904894"/>
+</node>
+</node>
+</node>
+<node TEXT="Software metrics" POSITION="left" ID="ID_1900492758" CREATED="1569681348992" MODIFIED="1569681356204">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Project planning" POSITION="right" ID="ID_1799111191" CREATED="1569681361411" MODIFIED="1569681372113">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Risk control" POSITION="left" ID="ID_1803848635" CREATED="1569681400386" MODIFIED="1569681408676">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="What is a project?" POSITION="right" ID="ID_389219915" CREATED="1569682279930" MODIFIED="1569682283411">
+<edge COLOR="#00ffff"/>
+<node TEXT="Dictionary definitions" ID="ID_544646747" CREATED="1569682297607" MODIFIED="1569682376286">
+<node ID="ID_1983569327" CREATED="1569682301655" MODIFIED="1569682421328"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A specific <b>plan</b>&#160;or design
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="A planned undertaking" ID="ID_1358811522" CREATED="1569682311089" MODIFIED="1569682319281"/>
+<node ID="ID_105442889" CREATED="1569682320262" MODIFIED="1569682428500"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A <b>large</b>&#160;undertaking
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="e.g. public works scheme" ID="ID_246794289" CREATED="1569682329271" MODIFIED="1569682382871"/>
+</node>
+</node>
+<node TEXT="Projects are inbetween jobs and exploration" ID="ID_678674821" CREATED="1569682434911" MODIFIED="1569682609139">
+<hook URI="img/jobs-and-exploration.png" SIZE="0.45600262" NAME="ExternalObject"/>
+<node TEXT="Jobs: repetition of well-defined, well understood tasks with no or little uncertainty" ID="ID_1554206446" CREATED="1569682617305" MODIFIED="1569682634268"/>
+<node TEXT="Exploration: the outcome, duration and budget are highly uncertain" ID="ID_1378002586" CREATED="1569682635690" MODIFIED="1569682658914">
+<node TEXT="e.g. finding a cure for a disease" ID="ID_1812495331" CREATED="1569682659336" MODIFIED="1569682663570"/>
+</node>
+</node>
+<node TEXT="Project-likeness indicators" ID="ID_455262785" CREATED="1569682706750" MODIFIED="1569682713769">
+<node TEXT="Non-routine" ID="ID_1863460486" CREATED="1569682714822" MODIFIED="1569682751575"/>
+<node TEXT="Large" ID="ID_622979612" CREATED="1569682894505" MODIFIED="1569682903771"/>
+<node TEXT="Complex" ID="ID_848959513" CREATED="1569682904345" MODIFIED="1569682905560"/>
+<node TEXT="Planned" ID="ID_46500550" CREATED="1569682756574" MODIFIED="1569682758878"/>
+<node TEXT="Specific target aimed at" ID="ID_1776103256" CREATED="1569682759052" MODIFIED="1569682767559"/>
+<node TEXT="Carried out for a customer" ID="ID_608730292" CREATED="1569682768485" MODIFIED="1569682778098"/>
+<node TEXT="Carried out by temp. work group" ID="ID_509888987" CREATED="1569682779261" MODIFIED="1569682786526"/>
+<node TEXT="Involving multiple specialities" ID="ID_237687143" CREATED="1569682787524" MODIFIED="1569682805690"/>
+<node TEXT="Split into multiple different phases" ID="ID_248609157" CREATED="1569682806235" MODIFIED="1569682830873"/>
+<node TEXT="Time and resource constraints" ID="ID_1785008886" CREATED="1569682831331" MODIFIED="1569682844179"/>
+</node>
+</node>
+<node TEXT="Project management activities" POSITION="right" ID="ID_1746082931" CREATED="1569683164802" MODIFIED="1569683312775">
+<edge COLOR="#7c0000"/>
+<hook URI="img/project-management-actions.png" SIZE="0.73550916" NAME="ExternalObject"/>
+<node TEXT="Feasibility study: Find whether the project is technically feasible and worthwhile for the business" ID="ID_533923493" CREATED="1569683202339" MODIFIED="1569683245665"/>
+<node TEXT="Execution: Implementation of plan, but that plan will likely change throughout project" ID="ID_954184240" CREATED="1569683246795" MODIFIED="1569683290512"/>
+</node>
+<node TEXT="Software life-cycle" POSITION="left" ID="ID_473136269" CREATED="1569683318163" MODIFIED="1569683323651">
+<edge COLOR="#00007c"/>
+<node TEXT="ISO 12207" ID="ID_1347083752" CREATED="1569683333444" MODIFIED="1569683636088">
+<hook URI="img/software-life-cycle.png" SIZE="0.8598734" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Project objectives" POSITION="left" ID="ID_1546007881" CREATED="1569683728067" MODIFIED="1569683731559">
+<edge COLOR="#007c00"/>
+<node TEXT="&quot;What do we have to do to have a successful project?&quot;" ID="ID_348670056" CREATED="1569683741823" MODIFIED="1569683770722"/>
+<node TEXT="Set by project authority" ID="ID_1333851325" CREATED="1569683774395" MODIFIED="1569684695758">
+<node TEXT="Sets scope of project" ID="ID_248774837" CREATED="1569683779096" MODIFIED="1569683784201"/>
+<node TEXT="Allocates and approves costs" ID="ID_972749016" CREATED="1569683784392" MODIFIED="1569683790570"/>
+<node TEXT="Could be an individual or a group" ID="ID_1977217509" CREATED="1569683791880" MODIFIED="1569683802118">
+<node TEXT="Project management board" ID="ID_326088762" CREATED="1569683803919" MODIFIED="1569683808230"/>
+<node TEXT="Steering committee" ID="ID_1059440796" CREATED="1569683808551" MODIFIED="1569683813164"/>
+</node>
+</node>
+<node TEXT="Post-conditions" ID="ID_857706069" CREATED="1569684652664" MODIFIED="1569684661782">
+<node TEXT="&quot;The project will be regarded as a success if...&quot;" ID="ID_605344134" CREATED="1569684663340" MODIFIED="1569684668905"/>
+<node TEXT="Focus what will be put in place over how activities will be carried out" ID="ID_1091415201" CREATED="1569684709023" MODIFIED="1569684868313">
+<node TEXT="Yay" ID="ID_1224807131" CREATED="1569684740483" MODIFIED="1569684804131">
+<node TEXT="A new payroll application doing X and Y will be operational by 4th April" ID="ID_239207570" CREATED="1569684805898" MODIFIED="1569684806884"/>
+</node>
+<node TEXT="Nooo" ID="ID_1088674955" CREATED="1569684809962" MODIFIED="1569684815881">
+<node TEXT="Design and implement a new payroll application" ID="ID_617262425" CREATED="1569684816258" MODIFIED="1569684825745">
+<font STRIKETHROUGH="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="SMART" ID="ID_1785664791" CREATED="1569684890015" MODIFIED="1569684895442">
+<node ID="ID_257048267" CREATED="1569684895448" MODIFIED="1569684955058"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>S</b>pecific: Concrete and well-defined
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_445019492" CREATED="1569684907144" MODIFIED="1569685023967"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>M</b>easurable: Achievement/satisfaction of objective can be judged objectively
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1069582478" CREATED="1569684998487" MODIFIED="1569685072322"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>A</b>chievable: Within abilities/power of individual or group concerned to meet the target
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_745467055" CREATED="1569685075968" MODIFIED="1569685125855"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>R</b>elevant: Objective must be relevant to the purpose of the project
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1785241608" CREATED="1569685126316" MODIFIED="1569685144055"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>T</b>ime constrained: Defined point in time by which objective should be achieved
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
