ddde24a25a1d00b05ca5c418ac5a7257916f1fba
Matthew Barber <quitesimplymatt@gmail.com>
2019-12-15 18:04:47 +0000

    moving stuff around nicer

--- CS3190 Information Security/2-cryptography.mm
+++ CS3190 Information Security/2-cryptography.mm
@@ -2,7 +2,7 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="Cryptography" FOLDED="false" ID="ID_1158788489" CREATED="1573922839297" MODIFIED="1573922859809" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle" zoom="0.565">
+<hook NAME="MapStyle" zoom="0.622">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
 
 <map_styles>
@@ -63,51 +63,13 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Concealing meaning" POSITION="right" ID="ID_397827764" CREATED="1570275259619" MODIFIED="1573922854447">
 <edge COLOR="#00007c"/>
 <hook URI="img/cryptosystem.png" SIZE="0.62953055" NAME="ExternalObject"/>
 </node>
-<node TEXT="Cryptosystem" POSITION="left" ID="ID_798536127" CREATED="1570275454311" MODIFIED="1573922871248">
-<edge COLOR="#ff0000"/>
-<node TEXT="Formal definition" ID="ID_810852466" CREATED="1570275844856" MODIFIED="1570275858060">
-<node TEXT="5-tuple (E, D, P, C, K)" ID="ID_926483721" CREATED="1570275503632" MODIFIED="1570275873319">
-<node TEXT="P: set of plaintext units" ID="ID_1629106931" CREATED="1570275555148" MODIFIED="1570275562826">
-<node TEXT="Symbols" ID="ID_1484573206" CREATED="1570275566140" MODIFIED="1570275576049">
-<node TEXT="&quot;a&quot;" ID="ID_317482638" CREATED="1570275588669" MODIFIED="1570275590264"/>
-<node TEXT="&quot;b&quot;" ID="ID_967811763" CREATED="1570275590846" MODIFIED="1570275592218"/>
-</node>
-<node TEXT="Symbol blocks" ID="ID_1070120614" CREATED="1570275576806" MODIFIED="1570275578979">
-<node TEXT="&quot;ab&quot;" ID="ID_213657398" CREATED="1570275580148" MODIFIED="1570275584559"/>
-<node TEXT="&quot;def&quot;" ID="ID_929396567" CREATED="1570275585204" MODIFIED="1570275586540"/>
-</node>
-</node>
-<node TEXT="C: set of ciphertext units" ID="ID_622090155" CREATED="1570275603767" MODIFIED="1570275609632"/>
-<node TEXT="K: set of possible keys" ID="ID_101311047" CREATED="1570275610117" MODIFIED="1570275614936"/>
-<node TEXT="E: encryption function" ID="ID_138418855" CREATED="1570275618029" MODIFIED="1570275631526">
-<node TEXT="P * K =&gt; C" ID="ID_663794533" CREATED="1570275779985" MODIFIED="1570275787475"/>
-</node>
-<node TEXT="D: decryption function" ID="ID_691022679" CREATED="1570275789806" MODIFIED="1570275797100">
-<node TEXT="C * K =&gt; P" ID="ID_1296086313" CREATED="1570275876568" MODIFIED="1570275882178"/>
-</node>
-</node>
-<node TEXT="D(E(p, k_e), k_d) = p" ID="ID_405546234" CREATED="1570276852403" MODIFIED="1570277075374">
-<hook URI="img/cryptosystem_with_formal_definition.png" SIZE="0.7251213" NAME="ExternalObject"/>
-<node TEXT="p =&gt; plaintext" ID="ID_1978624655" CREATED="1570277078197" MODIFIED="1570277089007"/>
-<node TEXT="k_[e,d] =&gt; key in set K_[e,d]" ID="ID_41893781" CREATED="1570277195386" MODIFIED="1570277222761"/>
-<node TEXT="E(p, k_e) =&gt; ciphertext" ID="ID_1046980862" CREATED="1570277027026" MODIFIED="1570277067448"/>
-</node>
-<node TEXT="e.g. shift cipher" ID="ID_1557467710" CREATED="1570279695426" MODIFIED="1570279782811">
-<hook URI="img/shift-cipher-example.png" SIZE="0.21367112" NAME="ExternalObject"/>
-<node TEXT="iamverypleasedto" ID="ID_329952844" CREATED="1570279900184" MODIFIED="1570279921883">
-<hook URI="img/shift-cipher-example-2.png" SIZE="0.1907239" NAME="ExternalObject"/>
-</node>
-<node TEXT="Caesar Cipher" ID="ID_1320470266" CREATED="1570279958465" MODIFIED="1570279964935">
-<node TEXT="Shift cipher with k = 3" ID="ID_332118834" CREATED="1570279965373" MODIFIED="1570279970966"/>
-</node>
-</node>
-</node>
-<node TEXT="Classifications" ID="ID_702017659" CREATED="1570280066159" MODIFIED="1570280069828">
+<node TEXT="Classifications of cryptosystems" POSITION="left" ID="ID_702017659" CREATED="1570280066159" MODIFIED="1574096768101">
+<edge COLOR="#0000ff"/>
 <node TEXT="Symmetry" ID="ID_544568978" CREATED="1570280242843" MODIFIED="1570280245355">
 <node TEXT="Symmetric" ID="ID_1087583456" CREATED="1570280070458" MODIFIED="1570280078034">
 <node TEXT="k_e = k_d" ID="ID_594445030" CREATED="1570280096051" MODIFIED="1570280170976"/>
@@ -165,17 +127,8 @@
 </node>
 </node>
 </node>
-<node TEXT="Good ciphers" ID="ID_541056293" CREATED="1570291181329" MODIFIED="1570291185881">
-<node TEXT="Properties" ID="ID_834737512" CREATED="1570291034197" MODIFIED="1570291177572">
-<node TEXT="Encryption &amp; decryption effort proportional to required secrecy" ID="ID_340827607" CREATED="1570291053856" MODIFIED="1570291071417"/>
-<node TEXT="Key selection and encryption process is simple enough for intended users" ID="ID_1183678806" CREATED="1570291073177" MODIFIED="1570291085683"/>
-<node TEXT="Easy to implement" ID="ID_641269485" CREATED="1570291086208" MODIFIED="1570291091502"/>
-<node TEXT="Errors in ciphering should not cascade to further corruption of information" ID="ID_395750688" CREATED="1570291092521" MODIFIED="1570291140812"/>
-</node>
-<node TEXT="Data Encryption Standard (DES)" ID="ID_377275930" CREATED="1570291192842" MODIFIED="1570291198928"/>
-<node TEXT="Advanced Encryption Standard (AES)" ID="ID_1703751839" CREATED="1570291208371" MODIFIED="1570291209349"/>
-</node>
-<node TEXT="Key management" ID="ID_459485885" CREATED="1570291427833" MODIFIED="1570291444491">
+<node TEXT="Key management" POSITION="right" ID="ID_459485885" CREATED="1570291427833" MODIFIED="1574096717260">
+<edge COLOR="#00ff00"/>
 <node TEXT="Symmetric-Key Ciphers" ID="ID_118038010" CREATED="1570291445144" MODIFIED="1570291451055">
 <node TEXT="k_e = k_d = k must be known only to sender &amp; reciever" ID="ID_450093499" CREATED="1570291464231" MODIFIED="1570291479412"/>
 <node TEXT="Different keys required for any pair of communicating parties" ID="ID_1836363458" CREATED="1570291491053" MODIFIED="1570291502673">
@@ -220,6 +173,54 @@
 <node TEXT="RSA" ID="ID_471863232" CREATED="1570292177553" MODIFIED="1570292180932"/>
 </node>
 </node>
+<node TEXT="Formal definition" POSITION="left" ID="ID_810852466" CREATED="1570275844856" MODIFIED="1574096720355">
+<edge COLOR="#ff00ff"/>
+<node TEXT="5-tuple (E, D, P, C, K)" ID="ID_926483721" CREATED="1570275503632" MODIFIED="1570275873319">
+<node TEXT="P: set of plaintext units" ID="ID_1629106931" CREATED="1570275555148" MODIFIED="1570275562826">
+<node TEXT="Symbols" ID="ID_1484573206" CREATED="1570275566140" MODIFIED="1570275576049">
+<node TEXT="&quot;a&quot;" ID="ID_317482638" CREATED="1570275588669" MODIFIED="1570275590264"/>
+<node TEXT="&quot;b&quot;" ID="ID_967811763" CREATED="1570275590846" MODIFIED="1570275592218"/>
+</node>
+<node TEXT="Symbol blocks" ID="ID_1070120614" CREATED="1570275576806" MODIFIED="1570275578979">
+<node TEXT="&quot;ab&quot;" ID="ID_213657398" CREATED="1570275580148" MODIFIED="1570275584559"/>
+<node TEXT="&quot;def&quot;" ID="ID_929396567" CREATED="1570275585204" MODIFIED="1570275586540"/>
+</node>
+</node>
+<node TEXT="C: set of ciphertext units" ID="ID_622090155" CREATED="1570275603767" MODIFIED="1570275609632"/>
+<node TEXT="K: set of possible keys" ID="ID_101311047" CREATED="1570275610117" MODIFIED="1570275614936"/>
+<node TEXT="E: encryption function" ID="ID_138418855" CREATED="1570275618029" MODIFIED="1570275631526">
+<node TEXT="P * K =&gt; C" ID="ID_663794533" CREATED="1570275779985" MODIFIED="1570275787475"/>
+</node>
+<node TEXT="D: decryption function" ID="ID_691022679" CREATED="1570275789806" MODIFIED="1570275797100">
+<node TEXT="C * K =&gt; P" ID="ID_1296086313" CREATED="1570275876568" MODIFIED="1570275882178"/>
+</node>
+</node>
+<node TEXT="D(E(p, k_e), k_d) = p" ID="ID_405546234" CREATED="1570276852403" MODIFIED="1570277075374">
+<hook URI="img/cryptosystem_with_formal_definition.png" SIZE="0.7251213" NAME="ExternalObject"/>
+<node TEXT="p =&gt; plaintext" ID="ID_1978624655" CREATED="1570277078197" MODIFIED="1570277089007"/>
+<node TEXT="k_[e,d] =&gt; key in set K_[e,d]" ID="ID_41893781" CREATED="1570277195386" MODIFIED="1570277222761"/>
+<node TEXT="E(p, k_e) =&gt; ciphertext" ID="ID_1046980862" CREATED="1570277027026" MODIFIED="1570277067448"/>
+</node>
+<node TEXT="e.g. shift cipher" ID="ID_1557467710" CREATED="1570279695426" MODIFIED="1570279782811">
+<hook URI="img/shift-cipher-example.png" SIZE="0.21367112" NAME="ExternalObject"/>
+<node TEXT="iamverypleasedto" ID="ID_329952844" CREATED="1570279900184" MODIFIED="1570279921883">
+<hook URI="img/shift-cipher-example-2.png" SIZE="0.1907239" NAME="ExternalObject"/>
+</node>
+<node TEXT="Caesar Cipher" ID="ID_1320470266" CREATED="1570279958465" MODIFIED="1570279964935">
+<node TEXT="Shift cipher with k = 3" ID="ID_332118834" CREATED="1570279965373" MODIFIED="1570279970966"/>
+</node>
+</node>
+</node>
+<node TEXT="Good ciphers" POSITION="left" ID="ID_541056293" CREATED="1570291181329" MODIFIED="1574096726350">
+<edge COLOR="#00ffff"/>
+<node TEXT="Properties" ID="ID_834737512" CREATED="1570291034197" MODIFIED="1570291177572">
+<node TEXT="Encryption &amp; decryption effort proportional to required secrecy" ID="ID_340827607" CREATED="1570291053856" MODIFIED="1570291071417"/>
+<node TEXT="Key selection and encryption process is simple enough for intended users" ID="ID_1183678806" CREATED="1570291073177" MODIFIED="1570291085683"/>
+<node TEXT="Easy to implement" ID="ID_641269485" CREATED="1570291086208" MODIFIED="1570291091502"/>
+<node TEXT="Errors in ciphering should not cascade to further corruption of information" ID="ID_395750688" CREATED="1570291092521" MODIFIED="1570291140812"/>
+</node>
+<node TEXT="Data Encryption Standard (DES)" ID="ID_377275930" CREATED="1570291192842" MODIFIED="1570291198928"/>
+<node TEXT="Advanced Encryption Standard (AES)" ID="ID_1703751839" CREATED="1570291208371" MODIFIED="1570291209349"/>
 </node>
 </node>
 </map>
