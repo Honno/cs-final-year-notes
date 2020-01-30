ae4895dc96754856ae7440f177653f382e101fa2
Matthew Barber <quitesimplymatt@gmail.com>
2020-01-30 17:02:07 +0000

    big data stuff for adv. db systems

---
+++ Advance Database Systems CS3800/0.5-introduction.mm
@@ -0,0 +1,324 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Introduction" FOLDED="false" ID="ID_865778923" CREATED="1580402285675" MODIFIED="1580402439199" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<node TEXT="Big Data" POSITION="right" ID="ID_1752325426" CREATED="1580402431651" MODIFIED="1580402434019">
+<edge COLOR="#ff0000"/>
+<node TEXT="History" ID="ID_1178699805" CREATED="1580402458598" MODIFIED="1580402855704"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &quot;A new revolution every fifteen years&quot;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="1. Personal Computers" ID="ID_431675737" CREATED="1580402500764" MODIFIED="1580402681076"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Accessibility of information processing
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Intel" ID="ID_1552253892" CREATED="1580402553355" MODIFIED="1580402730545"/>
+<node TEXT="AMD" ID="ID_442861216" CREATED="1580402730768" MODIFIED="1580402731735"/>
+<node TEXT="IBM" ID="ID_1718471214" CREATED="1580402731984" MODIFIED="1580402732891"/>
+<node TEXT="Apple" ID="ID_1263428050" CREATED="1580402733048" MODIFIED="1580402734690"/>
+<node TEXT="Microsoft" ID="ID_1281951049" CREATED="1580402734857" MODIFIED="1580402736161"/>
+<node TEXT="HP" ID="ID_903099520" CREATED="1580402737672" MODIFIED="1580402738730"/>
+<node TEXT="Dell" ID="ID_151528541" CREATED="1580402736495" MODIFIED="1580402737386"/>
+</node>
+<node TEXT="2. Internet" ID="ID_1294139770" CREATED="1580402509797" MODIFIED="1580402671044"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Information transmission capabilities skyrocket
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Yahoo" ID="ID_1915949950" CREATED="1580402631225" MODIFIED="1580402746580"/>
+<node TEXT="Google" ID="ID_279262246" CREATED="1580402746896" MODIFIED="1580402747843"/>
+<node TEXT="BAT" ID="ID_1404396839" CREATED="1580402748383" MODIFIED="1580402749273"/>
+<node TEXT="Baidu" ID="ID_494101879" CREATED="1580402749710" MODIFIED="1580402750695"/>
+<node TEXT="Alibaba" ID="ID_274893966" CREATED="1580402751191" MODIFIED="1580402755239"/>
+<node TEXT="Tencent" ID="ID_188906666" CREATED="1580402755812" MODIFIED="1580402758128"/>
+</node>
+<node TEXT="3. Big Data" ID="ID_569117913" CREATED="1580402597314" MODIFIED="1580402707424"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Explosion of information
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="" ID="ID_85340263" CREATED="1580402768992" MODIFIED="1580402768997">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Cloud Computing" ID="ID_753491772" CREATED="1580402527276" MODIFIED="1580402530603"/>
+<node TEXT="Internet of Things" ID="ID_312645744" CREATED="1580402531163" MODIFIED="1580402535057"/>
+<node TEXT="" ID="ID_1039471337" CREATED="1580402768989" MODIFIED="1580402768992">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Emerging enterprises" ID="ID_1037456561" CREATED="1580402768997" MODIFIED="1580402781490"/>
+</node>
+</node>
+</node>
+<node TEXT="Sources" ID="ID_1608718351" CREATED="1580402434825" MODIFIED="1580402868567"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Where does Big Data come from?
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="1. Business systems" ID="ID_119974526" CREATED="1580402876194" MODIFIED="1580402887451">
+<node TEXT="Data generated with business activities" ID="ID_483506394" CREATED="1580402901539" MODIFIED="1580402919181"/>
+<node TEXT="Relational databases" ID="ID_1232354520" CREATED="1580402927028" MODIFIED="1580402931439"/>
+<node TEXT="Centralised" ID="ID_575503472" CREATED="1580402922245" MODIFIED="1580402924808"/>
+<node TEXT="Passive" ID="ID_1031426334" CREATED="1580402925004" MODIFIED="1580402926046"/>
+</node>
+<node TEXT="2. User-generation" ID="ID_1685965432" CREATED="1580402888333" MODIFIED="1580402893152">
+<node TEXT="Data explosion with web 2.0, as opposed to servers" ID="ID_1128536389" CREATED="1580402942108" MODIFIED="1580403004868"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Users generating a lot of the content
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Fast data spreading" ID="ID_1282724954" CREATED="1580403033114" MODIFIED="1580403044517"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      via mobile internet capabilities
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Active" ID="ID_1913335536" CREATED="1580403046370" MODIFIED="1580403047676"/>
+</node>
+<node TEXT="3. Sensor networks" ID="ID_464285767" CREATED="1580402894029" MODIFIED="1580402898021">
+<node TEXT="Everything is a data generator" ID="ID_1857999312" CREATED="1580403051514" MODIFIED="1580403068164"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Thanks to <b>Internet of Things</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="RFID" ID="ID_1733272956" CREATED="1580403125977" MODIFIED="1580403127207"/>
+<node TEXT="Inevitable wave of Big Data" ID="ID_1271647419" CREATED="1580403083065" MODIFIED="1580403097229"/>
+<node TEXT="Streaming" ID="ID_108242500" CREATED="1580403098337" MODIFIED="1580403100485"/>
+</node>
+</node>
+<node TEXT="Properties of data" ID="ID_1973496422" CREATED="1580403150192" MODIFIED="1580403373511"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The &quot;Vs&quot; of Big Data
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="" ID="ID_410970228" CREATED="1580403183192" MODIFIED="1580403183194">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Volume" ID="ID_209660774" CREATED="1580403161534" MODIFIED="1580403430397"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bandwidth of data
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="" ID="ID_1557840696" CREATED="1580403452540" MODIFIED="1580403452541">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Data management" ID="ID_1778000758" CREATED="1580403431377" MODIFIED="1580403441456"/>
+<node TEXT="Data processing" ID="ID_428172505" CREATED="1580403446222" MODIFIED="1580403449051"/>
+<node TEXT="" ID="ID_1517609476" CREATED="1580403452534" MODIFIED="1580403452540">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Parallel" ID="ID_70235378" CREATED="1580403452542" MODIFIED="1580403456818"/>
+<node TEXT="Distributed" ID="ID_1378046476" CREATED="1580403457130" MODIFIED="1580403459740"/>
+</node>
+</node>
+<node TEXT="Velocity" ID="ID_1254349038" CREATED="1580403169789" MODIFIED="1580403511397"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Speed of data processing
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Streaming data management" ID="ID_1467673188" CREATED="1580403465882" MODIFIED="1580403471060"/>
+<node TEXT="Processing engine" ID="ID_945503224" CREATED="1580403471385" MODIFIED="1580403474179"/>
+</node>
+<node TEXT="Variety" ID="ID_1719047619" CREATED="1580403173408" MODIFIED="1580403527908"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Types of data
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Types" ID="ID_1649238413" CREATED="1580403610815" MODIFIED="1580403617429">
+<node TEXT="SMS" ID="ID_16595178" CREATED="1580403544551" MODIFIED="1580403579374"/>
+<node TEXT="XML" ID="ID_1287984262" CREATED="1580403581583" MODIFIED="1580403582708"/>
+<node TEXT="JSON" ID="ID_417595938" CREATED="1580403583007" MODIFIED="1580403584244"/>
+<node TEXT="Text" ID="ID_1979706697" CREATED="1580403584903" MODIFIED="1580403590159"/>
+<node TEXT="Image" ID="ID_1875320245" CREATED="1580403590575" MODIFIED="1580403591610"/>
+<node TEXT="Video" ID="ID_608569601" CREATED="1580403592271" MODIFIED="1580403593002"/>
+</node>
+<node TEXT="NoSQL" ID="ID_1589898476" CREATED="1580403537520" MODIFIED="1580403628977">
+<node TEXT="Not Only Sql" ID="ID_595633086" CREATED="1580403634365" MODIFIED="1580403637106"/>
+<node TEXT="NewSQL" ID="ID_48901874" CREATED="1580403637670" MODIFIED="1580403639477"/>
+</node>
+</node>
+<node TEXT="" ID="ID_554381525" CREATED="1580403183188" MODIFIED="1580403183192">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Chart" ID="ID_1574048198" CREATED="1580403183194" MODIFIED="1580403333433">
+<hook URI="img/big-data-axises.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
